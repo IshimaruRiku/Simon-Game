f2f3a50d099c5cd1c709b96bbc46ade03b356fec
Juandy Hartanto <juandyhartanto@gmail.com>
2019-11-30 20:16:27 +0700

    Simon Game Starting file
    Added starting file for simon game

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber = Math.ceil(Math.random() * 4);
+switch (randomNumber) {
+  case 1:
+    var randomColor = "green";
+    break;
+  case 2:
+    var randomColor = "red";
+    break;
+  case 3:
+    var randomColor = "blue";
+    break;
+  case 4:
+    var randomColor = "yellow";
+    break;
+  default:
+    break;
+}
+
+
+//Start Game by clicking one key
+$(document).keypress(event, function()
+{
+
+  $("#level-title").text()
+});
