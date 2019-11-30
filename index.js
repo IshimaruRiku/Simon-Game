de0ac6b6de45d09ce25ff4df00f0de0f92fe1ca3
Juandy Hartanto <juandyhartanto@gmail.com>
2019-11-30 23:03:58 +0700

    Finish change the gameflow and event handler

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-var randomNumber = Math.ceil(Math.random() * 4);
-switch (randomNumber) {
-  case 1:
-    var randomColor = "green";
-    break;
-  case 2:
-    var randomColor = "red";
-    break;
-  case 3:
-    var randomColor = "blue";
-    break;
-  case 4:
-    var randomColor = "yellow";
-    break;
-  default:
-    break;
-}
-
+var listColor = ["blue", "red", "yellow", "green"];
+var gameSequence = [];
+var userSequence = [];
 
 //Start Game by clicking one key
 $(document).keypress(event, function()
 {
-
-  $("#level-title").text()
+  if (gameSequence.length === 0)
+    nextLevel();
 });
+
+function nextLevel()
+{
+  userSequence = [];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomColor = listColor[randomNumber];
+  gameSequence.push(randomColor);
+  $("#level-title").text("Level " + gameSequence.length);
+  $("#" + randomColor).fadeOut(100).fadeIn(100);
+  playSound(randomColor);
+}
+
+$(".btn").on("click", function()
+{
+  var userColor = $(this).attr("id");
+  userSequence.push(userColor);
+  $(this).addClass("pressed");
+  setTimeout(function(){
+    $("#" + userColor).removeClass("pressed");
+  }, 100);
+  playSound(userColor);
+  checkColor(userSequence.length - 1);
+});
+
+function checkColor(level)
+{
+  if (gameSequence[level] == userSequence[level])
+  {
+    if (gameSequence.length === level + 1)
+    {
+      setTimeout(function(){
+        nextLevel();
+      }, 1000);
+    }
+  }
+  else
+  {
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 100);
+    playSound("wrong");
+    $("#level-title").text("Game Over, Press Any Key to Start");
+    gameSequence = [];
+  }
+}
+
+function playSound(sound)
+{
+  var audio = new Audio("sounds/" + sound + ".mp3");
+  audio.play();
+}
